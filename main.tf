9953b16d1127c22ebb187d4908e81d9333eba006
ashisnishanka <ak.nishanka@gmail.com>
2026-09-04 19:43:54 +0530

    added file now

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket-999"
+}
+
+# S3 Bucket Ownership Controls
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# S3 Bucket ACL
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+# Block Public Access
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Enable S3 Versioning
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# KMS Key
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  enable_key_rotation     = true
+  deletion_window_in_days = 7
+}
+
+# S3 Server-Side Encryption Using KMS
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
