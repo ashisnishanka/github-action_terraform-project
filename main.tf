73642515a85f2a76e78a6a256a1be5a664fd7bf3
ashisnishanka <ak.nishanka@gmail.com>
2026-09-04 20:22:13 +0530

    added file now2

--- main.tf
+++ main.tf
@@ -7,8 +7,17 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-tf-example-bucket-999"
 }
 resource "aws_s3_bucket" "log_bucket" {
-  bucket = "idream-log-test-bk"
+  bucket = "my-tf-example-bucket-999-logs1"
 }
+
+# Enable access logging
+resource "aws_s3_bucket_logging" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+}
+
 # S3 Bucket Ownership Controls
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.example.id
